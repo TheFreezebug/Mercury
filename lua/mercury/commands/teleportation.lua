0540ed7ec478d13f1ae3afeb033fd0318752aee6
TheFreezebug <danebush45@yahoo.com>
2014-12-02 13:20:22 -0500

    Commands galore!
    Added more commands

---
+++ lua/mercury/commands/teleportation.lua
@@ -0,0 +1,218 @@
+MCMD = {}
+MCMD.Command = "goto"
+MCMD.Verb = "gone to"
+MCMD.RconUse = true
+MCMD.Useage = "<player>"
+MCMD.UseImmunity = false
+MCMD.PlayerTarget = true
+MCMD.HasMenu = true
+
+
+
+function callfunc(caller,args)
+
+	caller:SetPos(args[1]:GetPos() + Vector(0,0,80) )
+
+
+
+	return true,"heh",false,{}
+	// First argument true / false -- Command succeeded? 
+	// Second argument: String error, if first argument is false this is pushed to the client.
+	// Third argument true / false -- supress default messages
+	// Fourth argument table, the message to print to chat if second argument is true.
+
+end
+
+
+function MCMD.GenerateMenu(frame)
+		local selectedplayer = nil 
+
+			local ctrl = vgui.Create( "DListView", frame)
+			ctrl:AddColumn( "Players" )
+			ctrl:SetSize( 210, 380 )	
+			ctrl:SetPos( 10, 0 )
+				
+
+			local DButtonRmsel = vgui.Create( "DButton" , frame)
+			DButtonRmsel:SetPos( 240, 40 )
+			DButtonRmsel:SetText( "GoTo" )
+			DButtonRmsel:SetSize( 130, 60 )
+			DButtonRmsel:SetDisabled(true)
+			DButtonRmsel.DoClick = function(self)
+				if self:GetDisabled()==true then return false end
+
+				surface.PlaySound("buttons/button3.wav")
+				net.Start("Mercury:Commands")
+					net.WriteString("goto")
+					net.WriteTable({selectedplayer})
+				net.SendToServer()
+				
+			end
+
+
+		
+			local players = player.GetAll()
+			local t = {}
+			for _, ply in ipairs( players ) do
+				local item = ctrl:AddLine( ply:Nick() )
+				item.ply = ply
+			end	
+
+			function ctrl:OnRowSelected(lineid,isselected)
+				local line_obj = self:GetLine(lineid)
+				surface.PlaySound("buttons/button6.wav")
+				selectedplayer = line_obj.ply
+				DButtonRmsel:SetDisabled(false)
+				return true
+			end
+end
+
+Mercury.Commands.AddCommand(MCMD.Command,MCMD,callfunc)
+
+
+
+MCMD = {}
+MCMD.Command = "bring"
+MCMD.Verb = "brought"
+MCMD.RconUse = true
+MCMD.Useage = "<player>"
+MCMD.UseImmunity = true
+MCMD.PlayerTarget = true
+MCMD.HasMenu = true
+
+
+
+function callfunc(caller,args)
+
+	args[1]:SetPos(caller:GetPos() + Vector(0,0,80) )
+
+
+
+	return true,"heh",false,{}
+	// First argument true / false -- Command succeeded? 
+	// Second argument: String error, if first argument is false this is pushed to the client.
+	// Third argument true / false -- supress default messages
+	// Fourth argument table, the message to print to chat if second argument is true.
+
+end
+
+
+function MCMD.GenerateMenu(frame)
+		local selectedplayer = nil 
+
+			local ctrl = vgui.Create( "DListView", frame)
+			ctrl:AddColumn( "Players" )
+			ctrl:SetSize( 210, 380 )	
+			ctrl:SetPos( 10, 0 )
+				
+
+			local DButtonRmsel = vgui.Create( "DButton" , frame)
+			DButtonRmsel:SetPos( 240, 40 )
+			DButtonRmsel:SetText( "Bring" )
+			DButtonRmsel:SetSize( 130, 60 )
+			DButtonRmsel:SetDisabled(true)
+			DButtonRmsel.DoClick = function(self)
+				if self:GetDisabled()==true then return false end
+	
+				surface.PlaySound("buttons/button3.wav")
+				net.Start("Mercury:Commands")
+					net.WriteString("bring")
+					net.WriteTable({selectedplayer})
+				net.SendToServer()
+				
+			end
+
+
+		
+			local players = player.GetAll()
+			local t = {}
+			for _, ply in ipairs( players ) do
+				local item = ctrl:AddLine( ply:Nick() )
+				item.ply = ply
+			end	
+
+			function ctrl:OnRowSelected(lineid,isselected)
+				local line_obj = self:GetLine(lineid)
+				surface.PlaySound("buttons/button6.wav")
+				selectedplayer = line_obj.ply
+				DButtonRmsel:SetDisabled(false)
+				return true
+			end
+end
+
+Mercury.Commands.AddCommand(MCMD.Command,MCMD,callfunc)
+
+--	target:SetPos(player:GetEyeTraceNoCursor().HitPos + Vector(0,0,72))
+
+
+
+MCMD = {}
+MCMD.Command = "tp"
+MCMD.Verb = "teleported"
+MCMD.RconUse = true
+MCMD.Useage = "<player>"
+MCMD.UseImmunity = true
+MCMD.PlayerTarget = true
+MCMD.HasMenu = true
+
+
+
+function callfunc(caller,args)
+
+	args[1]:SetPos(caller:GetEyeTraceNoCursor().HitPos + Vector(0,0,72))
+
+
+
+	return true,"heh",false,{}
+	// First argument true / false -- Command succeeded? 
+	// Second argument: String error, if first argument is false this is pushed to the client.
+	// Third argument true / false -- supress default messages
+	// Fourth argument table, the message to print to chat if second argument is true.
+
+end
+
+
+function MCMD.GenerateMenu(frame)
+		local selectedplayer = nil 
+
+			local ctrl = vgui.Create( "DListView", frame)
+			ctrl:AddColumn( "Players" )
+			ctrl:SetSize( 210, 380 )	
+			ctrl:SetPos( 10, 0 )
+				
+
+			local DButtonRmsel = vgui.Create( "DButton" , frame)
+			DButtonRmsel:SetPos( 240, 40 )
+			DButtonRmsel:SetText( "Teleport" )
+			DButtonRmsel:SetSize( 130, 60 )
+			DButtonRmsel:SetDisabled(true)
+			DButtonRmsel.DoClick = function(self)
+				if self:GetDisabled()==true then return false end
+	
+				surface.PlaySound("buttons/button3.wav")
+				net.Start("Mercury:Commands")
+					net.WriteString("tp")
+					net.WriteTable({selectedplayer})
+				net.SendToServer()
+				
+			end
+
+
+		
+			local players = player.GetAll()
+			local t = {}
+			for _, ply in ipairs( players ) do
+				local item = ctrl:AddLine( ply:Nick() )
+				item.ply = ply
+			end	
+
+			function ctrl:OnRowSelected(lineid,isselected)
+				local line_obj = self:GetLine(lineid)
+				surface.PlaySound("buttons/button6.wav")
+				selectedplayer = line_obj.ply
+				DButtonRmsel:SetDisabled(false)
+				return true
+			end
+end
+
+Mercury.Commands.AddCommand(MCMD.Command,MCMD,callfunc)
---
+++ lua/mercury/commands/utility.lua
@@ -0,0 +1,264 @@
+MCMD = {}
+MCMD.Command = "decals"
+MCMD.Verb = "cleaned up the decals"
+MCMD.RconUse = true
+MCMD.Useage = "<steamid> <time> <reason> "
+MCMD.UseImmunity = true
+MCMD.PlayerTarget = false
+MCMD.HasMenu = true
+
+
+
+function callfunc(caller,args)
+	for k,v in pairs(player.GetAll()) do
+		v:SendLua([[RunConsoleCommand("r_cleardecals")]])
+	end
+	
+
+
+	return true,"heh",false,{}
+	// First argument true / false -- Command succeeded? 
+	// Second argument: String error, if first argument is false this is pushed to the client.
+	// Third argument true / false -- supress default messages
+	// Fourth argument table, the message to print to chat if second argument is true.
+
+end
+
+
+function MCMD.GenerateMenu(frame)
+		local selectedplayer = nil 
+
+		 
+
+
+			local DButtonRmsel = vgui.Create( "DButton" , frame)
+			DButtonRmsel:SetPos( 10, 0 )
+			DButtonRmsel:SetText( "Clean decals" )
+			DButtonRmsel:SetSize( 130, 60 )
+
+			DButtonRmsel.DoClick = function(self)
+				//if self:GetDisabled()==true then return false end
+			//	local lid = ctrl:GetSelectedLine()
+				surface.PlaySound("buttons/button3.wav")
+				net.Start("Mercury:Commands")
+					net.WriteString("decals")
+					net.WriteTable({})
+				net.SendToServer()
+				 
+			end
+
+end
+
+Mercury.Commands.AddCommand(MCMD.Command,MCMD,callfunc)
+
+
+
+
+
+MCMD = {}
+MCMD.Command = "cleanup"
+MCMD.Verb = "cleaned up"
+MCMD.RconUse = true
+MCMD.Useage = "<player>"
+MCMD.UseImmunity = true
+MCMD.PlayerTarget = true
+MCMD.HasMenu = true
+
+
+
+function callfunc(caller,args)
+		local target = args[1]
+		local vm = target:GetViewModel( 0 ) 
+   		for k,v in pairs(ents.GetAll()) do
+                if v:CPPIGetOwner()==target and v~=vm then
+                        if !v:IsPlayer() and !v:IsWeapon() then
+                                v:Remove()
+                        end
+                end
+        end
+
+
+
+
+
+	return true,"heh",false,{}
+	// First argument true / false -- Command succeeded? 
+	// Second argument: String error, if first argument is false this is pushed to the client.
+	// Third argument true / false -- supress default messages
+	// Fourth argument table, the message to print to chat if second argument is true.
+
+end
+
+
+function MCMD.GenerateMenu(frame)
+		local selectedplayer = nil 
+
+			local ctrl = vgui.Create( "DListView", frame)
+			ctrl:AddColumn( "Players" )
+			ctrl:SetSize( 210, 380 )	
+			ctrl:SetPos( 10, 0 )
+				
+
+			local DButtonRmsel = vgui.Create( "DButton" , frame)
+			DButtonRmsel:SetPos( 240, 40 )
+			DButtonRmsel:SetText( "CleanUp" )
+			DButtonRmsel:SetSize( 130, 60 )
+			DButtonRmsel:SetDisabled(true)
+			DButtonRmsel.DoClick = function(self)
+				if self:GetDisabled()==true then return false end
+				local lid = ctrl:GetSelectedLine()
+				ctrl:RemoveLine(lid)	
+				surface.PlaySound("buttons/button3.wav")
+				net.Start("Mercury:Commands")
+					net.WriteString("cleanup")
+					net.WriteTable({selectedplayer})
+				net.SendToServer()
+				
+			end
+
+
+		
+			local players = player.GetAll()
+			local t = {}
+			for _, ply in ipairs( players ) do
+				local item = ctrl:AddLine( ply:Nick() )
+				item.ply = ply
+			end	
+
+			function ctrl:OnRowSelected(lineid,isselected)
+				local line_obj = self:GetLine(lineid)
+				surface.PlaySound("buttons/button6.wav")
+				selectedplayer = line_obj.ply
+				DButtonRmsel:SetDisabled(false)
+				return true
+			end
+end
+
+Mercury.Commands.AddCommand(MCMD.Command,MCMD,callfunc)
+
+
+
+
+
+
+
+
+
+
+
+MCMD = {}
+MCMD.Command = "god"
+MCMD.Verb = "enabled godmode for"
+MCMD.RconUse = true
+MCMD.Useage = "<player>"
+MCMD.UseImmunity = true
+MCMD.PlayerTarget = true
+MCMD.HasMenu = true
+
+
+
+function callfunc(caller,args)
+	args[1]:GodEnable()
+
+	return true,"",false,{} //RETURN CODES.
+	// First argument true / false -- Command succeeded? 
+	// Second argument: String error, if first argument is false this is pushed to the client.
+	// Third argument true / false -- supress default messages
+	// Fourth argument table, the message to print to chat if second argument is true.
+
+end
+
+
+function MCMD.GenerateMenu(frame)
+		local selectedplayer = nil 
+
+			local ctrl = vgui.Create( "DListView", frame)
+			ctrl:AddColumn( "Players" )
+			ctrl:SetSize( 210, 380 )	
+			ctrl:SetPos( 10, 0 )
+				
+			local UnGagButton = vgui.Create( "DButton" , frame)
+			local GagButton = vgui.Create( "DButton" , frame)
+			GagButton:SetPos( 240, 40 )
+			GagButton:SetText( "God" )
+			GagButton:SetSize( 130, 60 )
+			GagButton:SetDisabled(true)
+			GagButton.DoClick = function(self)
+				if self:GetDisabled()==true then return false end
+				surface.PlaySound("buttons/button3.wav")
+				net.Start("Mercury:Commands")
+					net.WriteString("god")
+					net.WriteTable({selectedplayer})
+				net.SendToServer()
+
+			end
+
+
+
+
+			UnGagButton:SetPos( 240, 120 )
+			UnGagButton:SetText( "UnGod" )
+			UnGagButton:SetSize( 130, 60 )
+			UnGagButton:SetDisabled(true)
+			UnGagButton.DoClick = function(self)
+				if self:GetDisabled()==true then return false end
+				surface.PlaySound("buttons/button3.wav")
+				net.Start("Mercury:Commands")
+					net.WriteString("ungod")
+					net.WriteTable({selectedplayer})
+				net.SendToServer()
+			
+			end
+
+
+		
+			local players = player.GetAll()
+			local t = {}
+			for _, ply in ipairs( players ) do
+				local item = ctrl:AddLine( ply:Nick() )
+				item.ply = ply
+			end	
+
+			function ctrl:OnRowSelected(lineid,isselected)
+				local line_obj = self:GetLine(lineid)
+				surface.PlaySound("buttons/button6.wav")
+				 UnGagButton:SetDisabled(false)
+				 GagButton:SetDisabled(false)
+				selectedplayer = line_obj.ply
+				return true
+			end
+end
+
+Mercury.Commands.AddCommand(MCMD.Command,MCMD,callfunc)
+
+
+
+
+MCMD = {}
+MCMD.Command = "ungod"
+MCMD.Verb = "disabled godmode for"
+MCMD.RconUse = true
+MCMD.Useage = "<player>"
+MCMD.UseImmunity = true
+MCMD.PlayerTarget = true
+MCMD.HasMenu = false
+
+
+
+function callfunc(caller,args)
+	args[1]:GodDisable()
+
+	return true,"",false,{} //RETURN CODES.
+	// First argument true / false -- Command succeeded? 
+	// Second argument: String error, if first argument is false this is pushed to the client.
+	// Third argument true / false -- supress default messages
+	// Fourth argument table, the message to print to chat if second argument is true.
+
+end
+
+
+function MCMD.GenerateMenu(frame)
+
+end
+
+Mercury.Commands.AddCommand(MCMD.Command,MCMD,callfunc)
